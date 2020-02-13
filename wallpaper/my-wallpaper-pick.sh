0ecb47c00057c2886dd231bae12149fedccc9229
Leixb <abone9999@gmail.com>
2020-02-13 12:20:41 +0100

    Change wallpaper folder

--- wallpaper/my-wallpaper-pick.sh
+++ wallpaper/my-wallpaper-pick.sh
@@ -1,3 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
-find . -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 feh --bg-max
+DIR="${HOME}/Pictures/Wallpapers/album"
+find "${DIR}" -type f \( -name '*.jpg' -o -name '*.png' \) -print0 | shuf -n1 -z | xargs -0 feh --no-xinerama --bg-fill
